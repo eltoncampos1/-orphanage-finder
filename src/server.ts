4b1e5f2718cf4a10a88d6a874f7a033838e9caf4
eltoncampos1 <eltoncampos36@gmail.com>
2020-12-17 09:08:04 -0300

    dealing with errors and finish aplication

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
 import path from 'path';
@@ -10,6 +11,7 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
